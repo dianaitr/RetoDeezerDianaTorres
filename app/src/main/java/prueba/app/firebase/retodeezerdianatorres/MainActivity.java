0b95b67a92054c1d0a37c19e8c31f25b90799b73
Diana Isabel Torres Rojas <diana.torres6@correo.icesi.edu.co>
2019-05-17 18:55:16 -0500

    Initial commit

---
+++ app/src/main/java/prueba/app/firebase/retodeezerdianatorres/MainActivity.java
@@ -0,0 +1,35 @@
+package prueba.app.firebase.retodeezerdianatorres;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+
+            case R.id.icon_return: {
+                //Intent i= new Intent(MainActivity.this,MainActivity.this);
+                break;
+            }
+        }
+        return true;
+    }
+}
